fbab70a1ba5bc9721d383048a1dfc6c7625487c7
cfmshine <cfmshine@163.com>
2016-08-08 20:18:55 +0800

    提交日志配置

--- src/test/java/com/cfmshine/test/DataSourceTest.java
+++ src/test/java/com/cfmshine/test/DataSourceTest.java
@@ -3,14 +3,17 @@ package com.cfmshine.test;
 import com.cfmshine.accuals.utils.ApplicationContextUtils;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import lombok.extern.log4j.Log4j;
 import org.junit.Test;
 
+@Log4j
 public class DataSourceTest {
 
     @Test
     public void DataSourceConnection() throws SQLException {
         DataSource dataSource = ApplicationContextUtils.getBean("dataSource");
         System.out.println(dataSource.getConnection());
+        log.info(dataSource + "info");
     }
 
 }
